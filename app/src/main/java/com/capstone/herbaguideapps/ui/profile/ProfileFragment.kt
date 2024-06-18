37be7fda16ab344abe9966ca3b38e60b42c543cd
Zeva22 <zeva.selsandra.m@mail.ukrim.ac.id>
2024-06-18 09:52:59 +0700

    Pofile

--- app/src/main/java/com/capstone/herbaguideapps/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/capstone/herbaguideapps/ui/profile/ProfileFragment.kt
@@ -5,16 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.capstone.herbaguideapps.R
+import com.capstone.herbaguideapps.databinding.FragmentHomeBinding
+import com.capstone.herbaguideapps.databinding.FragmentProfileBinding
+import com.capstone.herbaguideapps.session.SessionViewModel
+import com.capstone.herbaguideapps.utlis.factory.SessionViewModelFactory
 
 class ProfileFragment : Fragment() {
+
+    private val sessionViewModel by viewModels<SessionViewModel> {
+        SessionViewModelFactory.getInstance(requireActivity())
+    }
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
     }
 
-    companion object
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        sessionViewModel.getSession().observe(viewLifecycleOwner) { session ->
+            binding.txtName.text = session.name
+
+        }
+    }
+
 }
